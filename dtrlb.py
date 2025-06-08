274cc00afa75bd96428977ed4b9397fad9e54d41
libdmmy <itsdummybot@gmail.com>
2025-06-09 01:43:20 +0300

    working security log

--- dtrlb.py
+++ dtrlb.py
@@ -44,6 +44,8 @@ ENGLISH_LOCALE = {
 
     'forum.echo': 'echoes',
 
+    'security.untrusted_user': '⚠️ untrusted user detected ⚠️\n\nI banned him for you. my advice is to reset invite links.\n\nid: `{id}`\nname: `{name}`',
+
     'generic.starting': 'starting up!',
     'generic.topic_wasnt_found': 'creating {topic} topic for the first time...'
 }
@@ -52,13 +54,19 @@ RUSSIAN_LOCALE = complete_locale({
 
     'forum.echo': 'эха',
 
-    'generic.starting': 'стартуем!'
+    'security.untrusted_user': '⚠️ обнаружен неизвестный пользователь ⚠️\n\nвыкинул его из логова за тебя. мой совет таков - сбрось ссылки приглашений.\n\nайди: `{id}`\nимя: `{name}`',
+
+    'generic.starting': 'стартуем!',
+    'generic.topic_wasnt_found': 'впервые создаю топик {topic}...'
 })
 current_locale = RUSSIAN_LOCALE
 
 # === SOME LOGIC IDK ===
 def write_into_event_log(text: str):
-    pass
+    bot.send_message(config['tg_superchat_id'], text, parse_mode='Markdown', message_thread_id=config['forums']['event_log'])
+
+def handle_in_forum(codename: str):
+    return lambda m: m.message_thread_id == config['forums'][codename]
 
 # === BOT ===
 bot = TeleBot(config['tg_bot_token'])
@@ -72,23 +80,22 @@ def ensure_we_have_following_forum(codename: str, display_name: str):
         logger.info(current_locale['generic.topic_wasnt_found'].format(topic=codename))
         config['forums'][codename] = bot.create_forum_topic(lair_id, display_name).message_thread_id
 
+# === TOPICS ===
+
 ensure_we_have_following_forum('event_log', current_locale['forum.event_log'])
 ensure_we_have_following_forum('echo', current_locale['forum.echo'])
-
-@bot.chat_member_handler()
-def on_chat_member(event: ChatMemberUpdated):
-    if event.new_chat_member is None:
-        return
-    
-    if event.new_chat_member.user.id not in trusted_users:
-        bot.ban_chat_member(lair_id, event.new_chat_member.user.id, until_date=0)
-
-def handle_in_forum(codename: str):
-    return lambda m: m.message_thread_id == config['forums'][codename]
 
 @bot.message_handler(func=handle_in_forum('echo'))
 def on_echo_msg(msg: Message):
     bot.reply_to(msg, str(msg.text))
+
+# === SECURITY ===
+@bot.chat_member_handler(func=lambda e: e.new_chat_member is not None and e.new_chat_member.status == 'member')
+def on_chat_member(event: ChatMemberUpdated):
+    if event.new_chat_member.user.id not in trusted_users:
+        write_into_event_log(current_locale['security.untrusted_user'].format(id=event.new_chat_member.user.id,
+                                                                       name=event.new_chat_member.user.full_name))
+        bot.ban_chat_member(lair_id, event.new_chat_member.user.id, until_date=0)
 
 logger.info(current_locale['generic.starting'])
 bot.infinity_polling(allowed_updates=['message', 'chat_member'])
